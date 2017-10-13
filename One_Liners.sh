61b1f306e85a9ea1c4a25444fd286fce849d214f
Vijay Lakhujani <lakhujanivijay@gmail.com>
2017-10-13 09:56:33 +0530

    Update One_Liners.sh

--- One_Liners.sh
+++ One_Liners.sh
@@ -10,3 +10,7 @@ zcat whatever.fastq.gz | wc -l | awk '{print $1/4}'
 # Change extension of multiple files at once.
 # In below example, the extension changes from *.scafSeq to *.fa
 for f in *.scafSeq; do mv "$f" "$(basename "$f" .scafSeq).fa"; done
+
+
+# get A T G C counts for all sequences from a multi fasta file
+echo -e "seq_id\tA\tU\tG\tC"; while read line; do echo $line | grep ">" | sed 's/>//g'; for i in A U G C;do echo $line | grep -v ">" | grep -o $i | wc -l | grep -v "^0"; done; done < test.fa | paste - - - - -
