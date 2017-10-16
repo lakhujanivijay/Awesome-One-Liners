2fce86a6842b4e1208ec296a9e4750bf847ff6aa
Vijay Lakhujani <lakhujanivijay@gmail.com>
2017-10-16 09:55:38 +0530

    Update One_Liners.sh

--- One_Liners.sh
+++ One_Liners.sh
@@ -1,4 +1,4 @@
-# Awk_One_Liners
+# Shell one liners, using awk or something else!
 
 # Give a 2 column, tab separated list of read no. and read length
 zcat whatever.fastq.gz | paste - - - - | awk '{print NR " " (length($3))}'
@@ -14,3 +14,12 @@ for f in *.scafSeq; do mv "$f" "$(basename "$f" .scafSeq).fa"; done
 
 # get A T G C counts for all sequences from a multi fasta file
 echo -e "seq_id\tA\tU\tG\tC"; while read line; do echo $line | grep ">" | sed 's/>//g'; for i in A U G C;do echo $line | grep -v ">" | grep -o $i | wc -l | grep -v "^0"; done; done < test.fa | paste - - - - -
+
+#counting number of sequences in a fasta file:
+grep -c "^>" file.fa
+
+#add something to end of all header lines:
+sed 's/>.*/&WHATEVERYOUWANT/' file.fa > outfile.fa
+
+#clean up a fasta file so only first column of the header is outputted:
+awk '{print $1}' file.fa > output.fa
